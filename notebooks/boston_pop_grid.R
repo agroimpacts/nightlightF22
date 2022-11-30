e5132fa2b8b8300ccc7a0edcdd8589abb94d4893
lecarver <lcarver@clarku.edu>
2022-11-30 17:01:01 -0500

    Recreated public housing rds with transformed CRS to match other boston data

---
+++ notebooks/boston_pop_grid.R
@@ -0,0 +1,74 @@
+# create gridded population raster from Boston census data, as well as grid of
+# percentages of public housing
+
+library(sf)
+library(dplyr)
+library(terra)
+
+
+# Boston population raster
+bstntract <- readRDS("~/GeoSpaAR/nightlightF22/data/boston_2020census.rds")
+bstn_pop <- bstntract <- readRDS("~/GeoSpaAR/nightlightF22/data/bstn_pop.rds")
+bstn_ph <- bstntract <- readRDS("~/GeoSpaAR/nightlightF22/data/bstn_ph.rds")
+
+# Add area to census tracts then join population data
+nytract_pop <- nytract %>%
+  mutate(area = as.numeric(st_area(.) / 10^6)) %>%
+  mutate(BoroCT2020 = as.numeric(BoroCT2020)) %>%
+  inner_join(., ny_pop, by = "BoroCT2020")
+
+# Get nightlights grid
+nld <- rast(system.file("extdata/nightlights_mean.tif", package = "USFlite"))
+nyc <- readRDS(system.file("extdata/nyc.rds", package = "USFlite"))
+r <- nld[[1]]
+r[] <- 1:ncell(r)
+r <- mask(r, vect(nyc))
+p <- as.polygons(r) %>% st_as_sf()
+pgeo <- st_buffer(st_transform(p, st_crs(nytract_pop)), dist = 0) %>%
+  rename(cid = "2012-2021")
+# plot(p)
+
+# Intersect tracts with polygon grid and calculate area of segments
+# then calculate population within each 'cutup' census tract piece
+tracts_cut <- st_intersection(x = nytract_pop, y = pgeo) %>%
+  mutate(area_seg = as.numeric(st_area(.) / 10^6)) %>%
+  mutate(pop_seg = area_seg / area * Pop_20)
+
+# Calculate sum of population of tract segments within each cell
+# The 'layer' column indicates which cell index the tract segment belongs to
+r_pop_vals <- tracts_cut %>% group_by(cid) %>% st_drop_geometry() %>%
+  summarise(., pop = sum(pop_seg, na.rm = TRUE))
+# tracts_cut %>% slice(1:100) %>% select(cid) %>% plot()
+# tracts_cut %>% filter(cid == 216) %>% select(area) %>% plot()
+
+cids <- values(r)[!is.na(values(r))]
+# plot(p$geometry)
+# plot(p %>% filter(`2012-2021` == cids[!cids %in% r_pop_vals$cid]), add = TRUE,
+#      col = "red")
+
+plot(nytract_pop["Pop_20"])
+popr <- subst(r, from = r_pop_vals$cid, to = r_pop_vals$pop)
+popr[popr == cids[!cids %in% r_pop_vals$cid]] <- 0
+plot(popr)
+
+# Convert population to integer
+popr[] <- as.integer(popr[])
+
+writeRaster(popr, filename = "inst/extdata/nyc_pop.tif",
+            overwrite = TRUE)
+# popr <- rast("inst/extdata/nyc_pop.tif")
+
+# Create raster with proportion of pixel covered by public housing
+city_background <- popr
+city_background[city_background >= 0] <- 0
+
+ph_fraction <- exactextractr::coverage_fraction(
+  raster::raster(r), st_combine(nycha)
+)[[1]]
+ph_fraction[is.na(ph_fraction)] <- 0
+ph_fraction <- mask(rast(ph_fraction), city_background)
+# plot(ph_fraction)
+
+writeRaster(ph_fraction, filename = "inst/extdata/nyc_ph_coverage.tif",
+            overwrite = TRUE)
+
--- notebooks/dat_to_rds.R
+++ notebooks/dat_to_rds.R
@@ -15,17 +15,17 @@ sf_census <- read_sf("~/GeoSpaAR/nightlightF22/notebooks/extdata/SanFrancisco_20
 saveRDS(sf_census, file = "~/GeoSpaAR/nightlightF22/data/sf_2020census.rds")
 
 boston_publichousing <- read_csv("notebooks/extdata/boston_publichousing.csv")
-boston_ph <- st_as_sf(boston_publichousing, coords = c("Long", "Lat"), crs = 4326)
+boston_ph <-
+  st_as_sf(boston_publichousing, coords = c("Long", "Lat"), crs = 4326) %>%
+  st_transform(crs = st_crs(bstn_census))
 saveRDS(boston_ph, file = "~/GeoSpaAR/nightlightF22/data/boston_ph.rds")
 
 boston_grid<- raster("~/GeoSpaAR/nightlightF22/notebooks/extdata/boston_pop2010.tif")
 saveRDS(boston_grid, file = "~/GeoSpaAR/nightlightF22/data/bstn_grid.rds")
-
 
 sf_grid<- raster("~/GeoSpaAR/nightlightF22/notebooks/extdata/SF_gridpop_2010.tif")
 saveRDS(sf_grid, file = "~/GeoSpaAR/nightlightF22/data/sf_grid.rds")
 
 boston_pop <- read_csv("~/GeoSpaAR/nightlightF22/notebooks/extdata/bstn_pop.csv")
 saveRDS(boston_pop, file = "~/GeoSpaAR/nightlightF22/data/bstn_pop.rds")
-
 
