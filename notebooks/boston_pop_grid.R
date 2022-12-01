47201912511cba4cafb312a380a2aeb141a8952c
lecarver <lcarver@clarku.edu>
2022-12-01 17:06:04 -0500

    extract polygons of public housing & join census tract w/ pop data

--- notebooks/boston_pop_grid.R
+++ notebooks/boston_pop_grid.R
@@ -3,6 +3,7 @@
 
 library(sf)
 library(dplyr)
+library(tidyr)
 library(terra)
 library(here)
 library(sp)
@@ -14,15 +15,20 @@ bstn_pop <- readRDS(here("data/bstn_pop.RDS"))
 bstn_ph <- readRDS(here("data/bstn_ph.RDS"))
 bstn_bldg <- readRDS(here("data/bstn_bldg.RDS"))
 
-# Intersect Public Housing locations with Buildings[ to get Public Housing in polygon format
-bstn_phpoly <- bstn_bldg %>% mutate(AreaFt = st_area(.)) %>%
-  st_intersection(., bstn_ph)
+# Intersect Public Housing locations with Buildings to get Public Housing in polygon format
+bstn_intersect<- bstn_bldg  %>% st_intersects(st_combine(bstn_ph), .)
+bstn_php <- bstn_bldg %>% mutate(AreaFt = st_area(.)) %>%
+  slice(bstn_intersect[[1]])
+saveRDS(bstn_php, file = "~/GeoSpaAR/nightlightF22/data/bstn_phpoly.rds")
+
 
 # Add area to census tracts then join population data
-nytract_pop <- nytract %>%
-  mutate(area = as.numeric(st_area(.) / 10^6)) %>%
-  mutate(BoroCT2020 = as.numeric(BoroCT2020)) %>%
-  inner_join(., ny_pop, by = "BoroCT2020")
+bstn_pop2 <- bstn_pop[-1,] %>% mutate(TRACT = as.numeric(TRACT))
+bstntract_pop <- bstn_tract %>%
+  mutate(Area = as.numeric(units::set_units(st_area(.), "ft^2"))) %>%
+  mutate(TRACT = as.numeric(TRACTCE20)) %>%
+  inner_join(., bstn_pop2, by = "TRACT") %>%
+  rename(TotalPop = P0020001)
 
 # Get nightlights grid
 nld <- rast(system.file("extdata/nightlights_mean.tif", package = "USFlite"))
