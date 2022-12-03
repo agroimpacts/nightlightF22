c0af4f67dd70826a2073d43c80167cac27ef3a3e
lecarver <lcarver@clarku.edu>
2022-12-02 20:19:26 -0500

    Work on building pop grid

--- notebooks/boston_pop_grid.R
+++ notebooks/boston_pop_grid.R
@@ -19,7 +19,7 @@ bstn_bldg <- readRDS(here("data/bstn_bldg.RDS"))
 bstn_intersect<- bstn_bldg  %>% st_intersects(st_combine(bstn_ph), .)
 bstn_php <- bstn_bldg %>% mutate(AreaFt = st_area(.)) %>%
   slice(bstn_intersect[[1]])
-saveRDS(bstn_php, file = "~/GeoSpaAR/nightlightF22/data/bstn_phpoly.rds")
+saveRDS(bstn_php, file = here("data/bstn_phpoly.rds"))
 
 # Drop the column header row in census pop
 bstn_pop2 <- bstn_pop[-1,]
@@ -32,11 +32,20 @@ bstntract_pop <- bstn_tract %>%
 
 # Get nightlights grid
 nld <- rast(here("data/Boston_nightlights_mean.tif"))
-bstn <- readRDS(here("data/bstn.rds"))
+bstn <- readRDS(here("data/bstn.rds")) %>% st_transform(crs = 4326)
+# Create empty raster with the same properties as nld
 r <- nld[[1]]
 r[] <- 1:ncell(r)
-r <- mask(r, vect(bstn))
+# Mask to bstn, convert to polygons and project to Mass State Plane
+r <- mask(r, bstn)
 p <- as.polygons(r) %>% st_as_sf()
-pgeo <- st_buffer(st_transform(p, st_crs(nytract_pop)), dist = 0) %>%
+pgeo <- st_buffer(st_transform(p, st_crs(bstn_tract)), dist = 0) %>%
   rename(cid = "2012-2021")
-# plot(p)
+# plot(pgeo)
+# plot(bstn_tract %>% st_geometry(), add = TRUE)
+
+# Intersect tracts with polygon grid and calculate area of segments
+# then calculate population within each 'cutup' census tract piece
+tracts_cut <- st_intersection(x = bstntract_pop, y = pgeo) %>%
+  mutate(area_seg = as.numeric(st_area(.))) %>%
+  mutate(pop_seg = area_seg / area * TotalPop)
